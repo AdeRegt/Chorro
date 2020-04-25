9ab705db5adf5875d98e53bf89ab3f594b310714
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-25 15:00:29 +0200

    tab button problem fixed

--- Chorro-frontend/version1-chorro/src/components/LoginRegister/LoginRegister.js
+++ Chorro-frontend/version1-chorro/src/components/LoginRegister/LoginRegister.js
@@ -12,8 +12,6 @@ class LoginRegister extends React.Component {
     }
     
     register = () => {
-        console.log('alalalala');
-        
         this.props.switchRegister();
     }
     
@@ -25,20 +23,20 @@ class LoginRegister extends React.Component {
                     <div className="form-box">
                          <div className="button-box">
                              <div id="btn" style={{left: this.props.position_btn ? '110px': '15px'}}></div>
-                             <button type="button" className="toggle-btn" onClick={this.logIn}>Log  In</button>
-                             <button type="button" className="toggle-btn" onClick={this.register} >Register</button>
+                             <button type="button" className="toggle-btn" onClick={this.logIn} tabIndex="1">Log  In</button>
+                             <button type="button" className="toggle-btn" onClick={this.register} tabIndex="2" >Register</button>
                          </div>
 
-                         <form id="login" className="input-group" style={{left: this.props.position_login ? '-400px' : '50px'}}>
-                             <input type="text" className="input-field" placeholder="email" required/>
-                             <input type="text" className="input-field" placeholder="password" required/>
-                             <button type='submit' className="submit-btn">Login in</button>   
+                         <form id="login" className="input-group" style={{left: this.props.position_login ? '-400px' : '30px'}}>
+                             <input type="text" className="input-field" placeholder="email" tabIndex={this.props.tabLogInput1} required />
+                             <input type="text" className="input-field" placeholder="password" tabIndex={this.props.tabLogInput2} required/>
+                             <button type='submit' className="submit-btn" tabIndex={this.props.tabLogBtn}>Login in</button>   
                          </form>
 
-                         <form id="register" className="input-group" style={{left: this.props.position_register ? '50px' : '450px'}}>
-                             <input type="text" className="input-field" placeholder="email" required/>
-                             <input type="text" className="input-field" placeholder="password" required/>
-                             <button type='submit' className="submit-btn">Register</button>   
+                         <form id="register" className="input-group" style={{left: this.props.position_register ? '30px' : '450px'}}>
+                             <input type="text" className="input-field" placeholder="email" tabIndex={this.props.tabRegInput1} required/>
+                             <input type="text" className="input-field" placeholder="password" tabIndex={this.props.tabRegInput2} required/>
+                             <button type='submit' className="submit-btn" tabIndex={this.props.tabRegBtn}>Register</button>   
                          </form>
                          
                     </div>
@@ -53,7 +51,13 @@ const mapStateToProps = (state) => {
     return {
         position_login: state.auth.position_login,
         position_register: state.auth.position_register,
-        position_btn: state.auth.position_btn
+        position_btn: state.auth.position_btn,
+        tabLogInput1: state.auth.tabLogInput1,
+        tabLogInput2: state.auth.tabLogInput2,
+        tabLogBtn: state.auth.tabLogBtn,
+        tabRegInput1:  state.auth.tabRegInput1,
+        tabRegInput2: state.auth.tabRegInput2,
+        tabRegBtn: state.auth.tabRegBtn,    
      }
 }
 
--- Chorro-frontend/version1-chorro/src/reducers/authReducer.js
+++ Chorro-frontend/version1-chorro/src/reducers/authReducer.js
@@ -1,16 +1,40 @@
 import {SWITCH_LOGIN,SWITCH_REGISTER} from '../actions/types'
 
 const INIT_STATE = {
-    position_login: true,
-    position_register: true,
-    position_btn: true}
+    // to make login and register move
+    position_login: false,
+    position_register: false,
+    position_btn: false,
+    // skiping tab option button on the input and button that are hidden on the screen
+    tabLogInput1: "3",
+    tabLogInput2: "4",
+    tabLogBtn: "5",
+    tabRegInput1: "-1",
+    tabRegInput2: "-1",
+    tabRegBtn: "-1",
+
+}
 
 export default(state = INIT_STATE, action)=> {
     switch(action.type){
         case SWITCH_LOGIN: 
-            return{...state, position_login:false, position_register:false,position_btn:false};
+            return{...state, position_login:false, position_register:false, position_btn:false,
+                tabLogInput1: "3",
+                tabLogInput2: "4",
+                tabLogBtn: "5",
+                tabRegInput1: "-1",
+                tabRegInput2: "-1",
+                tabRegBtn: "-1",
+            };
         case SWITCH_REGISTER: 
-            return{...state, position_login:true, position_register:true,position_btn:true };
+            return{...state, position_login:true, position_register:true, position_btn:true,
+                tabLogInput1: "-1",
+                tabLogInput2: "-1",
+                tabLogBtn: "-1",
+                tabRegInput1: "3",
+                tabRegInput2: "4",
+                tabRegBtn: "5",
+            };
 
         default: return state
     }
