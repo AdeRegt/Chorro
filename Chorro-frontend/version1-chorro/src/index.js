b5856da216d7c8db2d6adc3f64868571d73971ff
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-26 14:33:35 +0200

    adding redux-dev tools &redux-thunk

--- Chorro-frontend/version1-chorro/src/index.js
+++ Chorro-frontend/version1-chorro/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import {createStore,applyMiddleware, compose} from 'redux';
+import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 const store = createStore(
-    reducers
+    reducers,composeEnhancers(applyMiddleware(reduxThunk))
 )
 
 ReactDOM.render(
