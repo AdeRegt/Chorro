683e8d42bfcf28484fe242ebcae7e29b7f11645f
AdeRegt <AdeRegt@github.com>
2020-05-15 21:14:17 +0200

    beginning of create_child function. translationfix on run file. adding changes to model and schema.

--- Chorro-backend/Chorro-backend/models/user.py
+++ Chorro-backend/Chorro-backend/models/user.py
@@ -1,6 +1,7 @@
 from freenit.models.sql.user import User as BaseUser
-
+from peewee import IntegerField
 
 class User(BaseUser):
     class Meta:
         table_name = 'users'
+    parent = IntegerField(null=True)
