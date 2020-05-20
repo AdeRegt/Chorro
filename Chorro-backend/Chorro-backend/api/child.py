857f5f75febcabfbc4114e9abd17c828cdb506a7
AdeRegt <AdeRegt@github.com>
2020-05-20 22:25:27 +0200

    adding function to add childuser to system.

--- Chorro-backend/Chorro-backend/api/child.py
+++ Chorro-backend/Chorro-backend/api/child.py
@@ -1,11 +1,14 @@
-from flask import current_app
 from flask_smorest import Blueprint, abort
-from flask.views import MethodView
-
-
+from freenit.api.methodviews import ProtectedMethodView
+from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+from flask import current_app, jsonify, request
 from marshmallow import fields
 from freenit.schemas.base import BaseSchema
-from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt_identity, create_access_token
+from flask_security.utils import hash_password
+import string
+import random
 
 #
 # This is the class with the child functions.
@@ -18,6 +21,7 @@ blueprint = Blueprint('child', 'child')
 
 class AddChildSchema(BaseSchema):
     childname = fields.Str(required=True, description='childname')
+    childemail = fields.Str(required=True, description='childemail')
 
 #
 # This item will add a child
@@ -28,32 +32,64 @@ class AddChildSchema(BaseSchema):
 # IN:
 #	childname		- name of the child
 @blueprint.route('/add', endpoint='add')
-class ChildAPI(MethodView):
+class ChildAPI(ProtectedMethodView):
 	@blueprint.arguments(AddChildSchema)
 	def post(self,args):
 	    		
 	 		# getting all fields
 			childname = args.get('childname', None)
+			email = args.get('childemail', None)
 		    	
-		    	# checking all fields
+		    # checking all fields
 			if childname is None:
 		    		abort(403, message='Argument childname is not defined while this is a required field')
+			# checking email
+			if email is None:
+		    		abort(403, message='Argument childemail is not defined while this is a required field')
 		    	
-		    	# checking if we are allowed to do this?
+		    # checking if we are allowed to do this?
 			User = current_app.user_datastore.user_model
+			idt = get_jwt_identity()
 			try:
-				parent = User.get(id=get_jwt_identity(),parent=None)
+				parent = User.get(id=idt,parent=None)
 			except User.DoesNotExist:
 		    		abort(403, message='This action is not allowed with current rights')
 		    	
-		    	# checking if there is a child like this already
+			# random hash
+			rnd = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(30))
+
+		    # checking if there is a child like this already
 			try:
-				child = User.get(name=childname,parent=get_jwt_identity())
+				child = User.get(name=childname,parent=idt)
 				abort(403, message='This child does already exist')
 			except User.DoesNotExist:
-		    		child = User()
-		    		child.save()
-		    		
+				child = User()
+			
+			child.parent = idt
+			child.name = childname
+			child.email = email
+			child.active = True
+			child.password = hash_password(rnd)
+			child.save()
+
+			expires = timedelta(
+				hours=current_app.config['PASSWORD_RESET_EXPIRY'],
+			)
+			identity = {
+				'id': child.id,
+				'reset': True,
+			}
+			host = request.headers.get('Origin', request.url_root)
+			resetToken = create_access_token(identity, expires_delta=expires)
+			url = f'{host}/reset/{resetToken}'
+			msg = MIMEText(url, 'plain', 'utf-8')
+			msg['From'] = 'office@example.com'
+			subjects = current_app.config['SUBJECTS']
+			subject = subjects['prefix'] + subjects['register']
+			msg['Subject'] = subject
+			msg['To'] = child.email
+			current_app.sendmail(msg)
+
 			# declare victory
 			answer = jsonify({
 				'success':True
--- Chorro-backend/Chorro-backend/models/user.py
+++ Chorro-backend/Chorro-backend/models/user.py
@@ -1,7 +1,8 @@
 from freenit.models.sql.user import User as BaseUser
-from peewee import IntegerField
+import peewee as pw
 
 class User(BaseUser):
+    parent = pw.IntegerField(null=True,db_column="parent")
+    name = pw.TextField(null=True,db_column="name")
     class Meta:
         table_name = 'users'
-    parent = IntegerField(null=True)
